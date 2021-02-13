65c373053e6c561efbccb270460734b4f8a9829a
Ossi Bister <ossi@locals-mbp.lan>
2021-02-13 19:15:45 +0200

    Initial commit

---
+++ src/main/java/fi/fmi/weather/WeatherApplication.java
@@ -0,0 +1,13 @@
+package fi.fmi.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+}
